67a0460f6df2aeeaa3a661cec38e184557690d3d
SardorDev12 <wiut00012453@gmail.com>
2022-10-29 09:42:14 +0500

    done: some codewar katas

--- 8kyu/strangeTripToTheMarket.js
+++ 8kyu/strangeTripToTheMarket.js
@@ -1,7 +1,3 @@
-git status
-
-
-N:
 // You're on your way to the market when you hear beautiful music coming from a nearby street performer. The notes come together like you wouln't believe as the musician puts together patterns of tunes. As you wonder what kind of algorithm you could use to shift octaves by 8 pitches or something silly like that, it dawns on you that you have been watching the musician for some 10 odd minutes. You ask, "how much do people normally tip for something like this?" The artist looks up. "It's always gonna be about tree fiddy."
 
 // It was then that you realize the musician was a 400 foot tall beast from the paleolithic era! The Loch Ness Monster almost tricked you!
@@ -11,7 +7,7 @@ N:
 // Since Nessie is a master of disguise, the only way accurately tell is to look for the phrase "tree fiddy". Since you are tired of being grifted by this monster, the time has come to code a solution for finding The Loch Ness Monster. Note that the phrase can also be written as "3.50" or "three fifty".
 
 //My solution
-function isLockNessMonster(s) {
+N: function isLockNessMonster(s) {
   return (
     s.includes("tree fiddy") || s.includes("3.50") || s.includes("three fifty")
   );
